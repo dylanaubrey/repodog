f7ac3866982611a415075ccbe7757411646447c5
Dylan Aubrey <dylanaubrey@gmail.com>
2019-09-03 08:24:26 +0100

    feat(new-monorepo): adding pause to check repo before install

--- packages/new-monorepo/src/main.ts
+++ packages/new-monorepo/src/main.ts
@@ -229,15 +229,15 @@ export default async function newMonorepo() {
     rootPackageJson = loadRootPackageJson();
 
     if (!rootPackageJson) {
-      return error("Repodog expected a package.json to exist in the project root.");
+      return error("Repodog expected a package.json to exist in the project root");
     }
 
     if (!rootPackageJson.name || !validatePackageName(rootPackageJson.name).valid) {
-      return error("Repodog expected the project package.json to have a valid name.");
+      return error("Repodog expected the project package.json to have a valid name");
     }
 
     if (!rootPackageJson.version || !semver.valid(rootPackageJson.version)) {
-      return error("Repodog expected the project package.json to have a valid version.");
+      return error("Repodog expected the project package.json to have a valid version");
     }
 
     const { features } = await inquirer.prompt({
@@ -251,6 +251,17 @@ export default async function newMonorepo() {
 
     info("Copying scaffold to new monorepo");
     await iterateDirectory(resolvePathToCwd(SCAFFOLD_DIR_PATH), copyFiles(resolvePathToCwd(".")), { sync: true });
+
+    const { proceed } = await inquirer.prompt({
+      message: "Are the files in your repository correct?",
+      name: "proceed",
+      type: "confirm",
+    });
+
+    if (!proceed) {
+      return error("The files in your repository are not correct");
+    }
+
     exec("yarn");
     const includedPackages = getIncludedPackages();
     exec(`yarn add ${includedPackages.join(" ")} --dev -W`);

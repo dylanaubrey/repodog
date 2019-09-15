e16463f0035cc2c9b0f7453ceb056dee5b81eb41
Dylan Aubrey <dylanaubrey@gmail.com>
2019-09-15 21:54:09 +0100

    fix(jest, tsconfig): update configs to resolve install bugs

--- packages/new-repo/scaffold/jest.config.js
+++ packages/new-repo/scaffold/jest.config.js
@@ -2,4 +2,6 @@ const repodogConfig = require('@repodog/jest-config');
 
 module.exports = {
   ...repodogConfig,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/types.ts', '!**/*.test.*', '!**/__test__/**'],
+  testMatch: ['<rootDir>/src/**/*.test.*'],
 };

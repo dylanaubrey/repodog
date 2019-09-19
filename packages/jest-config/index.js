29cffa9874b33e0e66dee34fd9393944f8fe08c7
Dylan Aubrey <dylanaubrey@gmail.com>
2019-09-19 07:29:16 +0100

    fix(jest): add default test ignore pattern

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -3,6 +3,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'lcov', 'text-summary'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['/__snapshots__/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },

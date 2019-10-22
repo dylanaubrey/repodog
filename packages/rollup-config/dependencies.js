25eee86bf48abc6ee77531cecb6c5190879377db
Dylan Aubrey <dylanaubrey@gmail.com>
2019-10-22 08:35:34 +0100

    feat: update rollup config to fix bugs and add dependencies

---
+++ packages/rollup-config/dependencies.js
@@ -0,0 +1,13 @@
+const core = [
+  'rollup',
+  'rollup-plugin-analyzer',
+  'rollup-plugin-babel',
+  'rollup-plugin-commonjs',
+  'rollup-plugin-json',
+  'rollup-plugin-node-resolve',
+  'rollup-plugin-terser',
+];
+
+module.exports = {
+  core,
+};
--- packages/rollup-config/index.js
+++ packages/rollup-config/index.js
@@ -1,3 +1,4 @@
+const appRoot = require('app-root-path');
 const { outputFileSync } = require('fs-extra');
 const { basename, resolve } = require('path');
 const { plugin: analyzer } = require('rollup-plugin-analyzer');
@@ -6,15 +7,33 @@ const commonjs = require('rollup-plugin-commonjs');
 const json = require('rollup-plugin-json');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const { terser } = require('rollup-plugin-terser');
-const rootPackageJson = require('../../../package.json'); // eslint-disable-line import/no-unresolved
+
+let rootPackageJson;
+
+try {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  rootPackageJson = require(resolve(appRoot.toString(), 'package.json'));
+} catch (error) {
+  // eslint-disable-next-line no-console
+  console.error(`No package.json found in in root directory ${appRoot.toString()}`, error);
+}
 
 const packageDir = resolve(process.cwd());
-const packageJson = require(`${packageDir}/package.json`); // eslint-disable-line import/no-dynamic-require
+let currentPackageJson;
 
-const devDependencies = Object.keys(rootPackageJson.devDependencies);
-const peerDependencies = Object.keys(packageJson.peerDependencies);
-const dependencies = Object.keys(packageJson.dependencies);
-const externalModuleNames = [...dependencies, ...peerDependencies, ...devDependencies];
+try {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  currentPackageJson = require(`${packageDir}/package.json`);
+} catch (error) {
+  // eslint-disable-next-line no-console
+  console.error(`No package.json found in directory ${packageDir}`, error);
+}
+
+const rootDevDependencies = Object.keys(rootPackageJson.devDependencies || {});
+const devDependencies = Object.keys(currentPackageJson.devDependencies || {});
+const peerDependencies = Object.keys(currentPackageJson.peerDependencies || {});
+const dependencies = Object.keys(currentPackageJson.dependencies || {});
+const externalModuleNames = [...rootDevDependencies, ...dependencies, ...peerDependencies, ...devDependencies];
 
 function external(id) {
   return externalModuleNames.some(name => id.startsWith(name));
@@ -25,6 +44,7 @@ const extensions = ['.mjs', '.js', '.jsx', 'json', '.ts', '.tsx'];
 const defaultPlugins = [
   nodeResolve({
     extensions,
+    preferBuiltins: true,
   }),
   commonjs(),
   json(),
@@ -35,39 +55,40 @@ const defaultPlugins = [
   }),
 ];
 
-function writeTo(analysisString) {
-  outputFileSync(`${packageDir}/lib/browser/production.analysis.txt`, analysisString);
-}
-
-const dirName = basename(packageDir);
-
 function sourcemapPathTransform(sourcePath) {
   if (/node_modules/.test(sourcePath)) return sourcePath;
-  return sourcePath.replace('../../src/', `../${dirName}/src/`);
+  return sourcePath.replace('../../src/', `../${basename(packageDir)}/src/`);
 }
 
-const devConfig = {
-  external,
-  input: `${packageDir}/src/index`,
-  output: {
-    file: `${packageDir}/lib/browser/index.js`,
-    format: 'esm',
-    sourcemap: true,
-    sourcemapPathTransform,
-  },
-  plugins: [...defaultPlugins],
-};
-
-const prodConfig = {
-  external,
-  input: `${packageDir}/src/index`,
-  output: {
-    file: `${packageDir}/lib/browser/index.js`,
-    format: 'esm',
-    sourcemap: true,
-    sourcemapPathTransform,
-  },
-  plugins: [...defaultPlugins, terser(), analyzer({ writeTo })],
-};
-
-module.exports = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
+module.exports =
+  process.env.NODE_ENV === 'development'
+    ? {
+        external,
+        input: `${packageDir}/src/index`,
+        output: {
+          file: `${packageDir}/lib/browser/index.js`,
+          format: 'esm',
+          sourcemap: true,
+          sourcemapPathTransform,
+        },
+        plugins: [...defaultPlugins],
+      }
+    : {
+        external,
+        input: `${packageDir}/src/index`,
+        output: {
+          file: `${packageDir}/lib/browser/index.js`,
+          format: 'esm',
+          sourcemap: true,
+          sourcemapPathTransform,
+        },
+        plugins: [
+          ...defaultPlugins,
+          terser(),
+          analyzer({
+            writeTo: analysisString => {
+              outputFileSync(`${packageDir}/lib/browser/production.analysis.txt`, analysisString);
+            },
+          }),
+        ],
+      };
